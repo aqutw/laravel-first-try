b17db2722f381029fb4f6299013ff16019f31312
aqu <aqu@aqu.com>
2019-10-05 14:50:30 +0800

    play routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,37 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+define('PROFILE_ROUTE_NAME','profile');
+define('ADMIN_ROUTE_NAME_PREFIX','admin.');
+define('USER_LIST_ROUTE_NAME','names');
 
 Route::get('/', function () {
-    return view('welcome');
+    echo'xxx';
+});
+
+// Route::get('/{psyname}', function ($name) {
+//     echo$name;
+// });
+
+Route::get('search/{rest}', function ($rest) {
+    echo $rest;
+})->where('rest', '.*');
+
+Route::any('/', function () {
+    echo'Handling all HTTP methods at "/"';
+});
+
+Route::name(PROFILE_ROUTE_NAME)->get('user/profile', function () {
+    echo'user profile here:';
+    echo route('profile');
+});
+
+Route::name(ADMIN_ROUTE_NAME_PREFIX)->group(function () {
+    Route::get('users', function () {
+        // Route assigned name "admin.users"...
+    })->name(USER_LIST_ROUTE_NAME);
+});
+
+Route::fallback(function () {
+    echo'404 page here.';
 });
